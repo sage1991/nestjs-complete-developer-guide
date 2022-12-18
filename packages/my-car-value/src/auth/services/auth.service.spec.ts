f67b80e53ef2d57ddc6b69a8bbf0c990782aeeed
harry <shsage91@gmail.com>
2022-12-18 19:06:46 +0900

    Implements unit test for auth service

--- packages/my-car-value/src/auth/services/auth.service.spec.ts
+++ packages/my-car-value/src/auth/services/auth.service.spec.ts
@@ -1,12 +1,51 @@
 import { Test, TestingModule } from "@nestjs/testing"
+import { BadRequestException, NotFoundException } from "@nestjs/common"
+
 import { AuthService } from "./auth.service"
+import { UsersService } from "../../users/services"
+import { CreateUserRequest, FindUserRequest, UpdateUserRequest } from "../../users/models"
+import { UserEntity } from "../../users/entities"
 
 describe("AuthService", () => {
   let service: AuthService
+  const fakeUsersServiceFactory = (): Partial<UsersService> => {
+    const users: UserEntity[] = []
+    return {
+      findAll(request: FindUserRequest) {
+        const { email } = request ?? {}
+        return Promise.resolve(users.filter((user) => (email ? user.email === email : true)))
+      },
+      create(request: CreateUserRequest) {
+        const user = { id: users.length, ...request }
+        users.push(user)
+        return Promise.resolve(user)
+      },
+      findOne(id: number) {
+        return Promise.resolve(users.find((user) => user.id === id))
+      },
+      remove(id: number) {
+        return Promise.resolve({ id, email: "", password: "" })
+      },
+      update(id: number, request: UpdateUserRequest) {
+        const user = users.find((user) => user.id === id)
+        if (user) {
+          user.email = request.email ?? user.email
+          user.password = request.password ?? user.password
+        }
+        return Promise.resolve(user)
+      }
+    }
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService]
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useFactory: fakeUsersServiceFactory
+        }
+      ]
     }).compile()
 
     service = module.get<AuthService>(AuthService)
@@ -14,5 +53,66 @@ describe("AuthService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined()
+  })
+
+  it("creates a new user with salted and hashed password", async () => {
+    const user = await service.signup({
+      email: "harry.kane@kakaostyle.com",
+      password: "12341234"
+    })
+    const { password } = user
+    const [salt, hash] = password.split(".")
+    expect(password).not.toEqual("12341234")
+    expect(salt).toBeDefined()
+    expect(hash).toBeDefined()
+  })
+
+  it("throws an BadRequestException if user signs up with email in use", async () => {
+    await service.signup({
+      email: "harry.kane@kakaostyle.com",
+      password: "12341234"
+    })
+    const signup = () =>
+      service.signup({
+        email: "harry.kane@kakaostyle.com",
+        password: "12341234"
+      })
+    await expect(signup).rejects.toThrow(BadRequestException)
+  })
+
+  it("throws an NotFoundException if signin is called with an unused email", async () => {
+    const signin = () =>
+      service.signin({
+        email: "harry.kane@kakaostyle.com",
+        password: "12341234"
+      })
+    await expect(signin).rejects.toThrow(NotFoundException)
+  })
+
+  it("throws an BadRequestException if invalid password is provided", async () => {
+    await service.signup({
+      email: "harry.kane@kakaostyle.com",
+      password: "12341234"
+    })
+
+    const signin = () =>
+      service.signin({
+        email: "harry.kane@kakaostyle.com",
+        password: "123"
+      })
+    await expect(signin).rejects.toThrow(BadRequestException)
+  })
+
+  it("returns a user if correct password is provided", async () => {
+    await service.signup({
+      email: "harry.kane@kakaostyle.com",
+      password: "12341234"
+    })
+
+    const user = await service.signin({
+      email: "harry.kane@kakaostyle.com",
+      password: "12341234"
+    })
+    expect(user).toBeDefined()
   })
 })
